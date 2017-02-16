d4842511677bc1c74ab2f7babd01df087fe20a96
clint shryock <clint@ctshryock.com>
2017-02-16 10:01:01 -0600

    stub out calls to trace flow

--- builtin/providers/aws/resource_aws_vpc.go
+++ builtin/providers/aws/resource_aws_vpc.go
@@ -91,6 +91,7 @@ func resourceAwsVpc() *schema.Resource {
 }
 
 func resourceAwsVpcCreate(d *schema.ResourceData, meta interface{}) error {
+	d.Timeout("create")
 	conn := meta.(*AWSClient).ec2conn
 	instance_tenancy := "default"
 	if v, ok := d.GetOk("instance_tenancy"); ok {
@@ -137,6 +138,7 @@ func resourceAwsVpcCreate(d *schema.ResourceData, meta interface{}) error {
 }
 
 func resourceAwsVpcRead(d *schema.ResourceData, meta interface{}) error {
+	d.Timeout("read")
 	conn := meta.(*AWSClient).ec2conn
 
 	// Refresh the VPC state
@@ -242,6 +244,7 @@ func resourceAwsVpcRead(d *schema.ResourceData, meta interface{}) error {
 
 func resourceAwsVpcUpdate(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*AWSClient).ec2conn
+	d.Timeout("update")
 
 	// Turn on partial mode
 	d.Partial(true)
@@ -325,6 +328,7 @@ func resourceAwsVpcUpdate(d *schema.ResourceData, meta interface{}) error {
 }
 
 func resourceAwsVpcDelete(d *schema.ResourceData, meta interface{}) error {
+	d.Timeout("delete")
 	conn := meta.(*AWSClient).ec2conn
 	vpcID := d.Id()
 	DeleteVpcOpts := &ec2.DeleteVpcInput{
